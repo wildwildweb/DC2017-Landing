26f85c1ed777241b4678aaec5f6e51fa99b53c61
Javi DC <javi@wildwildweb.es>
2016-07-29 18:49:11 +0200

    Ajustes responsive 2.0

--- js/main.js
+++ js/main.js
@@ -23,6 +23,47 @@ jQuery(document).ready(function($) {
                     $('.ico.thunder').addClass('bounceOutLeft').removeClass('bounceInLeft');
                     $('.ico.mobile').addClass('bounceOutRight').removeClass('bounceInRight');
                 }
+				
+                setTimeout(
+                    function() {
+                        if (nextIndex == 3) {
+							$('.name').addClass('fadeInLeft').removeClass('fadeOutLeft');
+                        }
+                    },
+                    100
+                );
+                if (index == 3) {
+					$('.name').addClass('fadeOutLeft').removeClass('fadeInLeft');
+                }
+				
+                setTimeout(
+                    function() {
+                        if (nextIndex == 4) {
+							$('.name').addClass('fadeInLeft').removeClass('fadeOutLeft');
+                        }
+                    },
+                    100
+                );
+                if (index == 4) {
+					$('.name').addClass('fadeOutLeft').removeClass('fadeInLeft');
+                }
+				
+                setTimeout(
+                    function() {
+                        if (nextIndex == 5) {
+							$('.name').addClass('fadeInLeft').removeClass('fadeOutLeft');
+							$('.ico.code').addClass('bounceInDown').removeClass('bounceOutUp');
+							$('.ico.newsletter').addClass('bounceInUp').removeClass('bounceOutDown');
+                        }
+                    },
+                    100
+                );
+                if (index == 5) {
+					$('.name').addClass('fadeOutLeft').removeClass('fadeInLeft');
+					$('.ico.code').addClass('bounceOutUp').removeClass('bounceInDown');
+					$('.ico.newsletter').addClass('bounceOutDown').removeClass('bounceInUp');
+                }
+				
             },
             afterRender: function(){
 				setTimeout (
