181cc053ffeb4e45577e78ae7010a11c4a23e51a
Cesar <cesar@MacBook-Pro-3.local>
2016-08-03 14:12:54 +0200

    Remove animations, etc
    - Remove animate.css
    - Change svg
    - Remove old stuff
    - Responsive changes
    - and much more

--- js/main.js
+++ js/main.js
@@ -7,64 +7,6 @@ jQuery(document).ready(function($) {
             navigation: true,
             responsiveWidth: 980,
             responsiveHeight: 750,
-            onLeave: function(index, nextIndex, direction) {
-                setTimeout(
-                    function() {
-                        if (nextIndex == 2) {
-							$('.name').addClass('fadeInLeft').removeClass('fadeOutLeft');
-                            $('.ico.thunder').addClass('bounceInLeft').removeClass('bounceOutLeft');
-                            $('.ico.mobile').addClass('bounceInRight').removeClass('bounceOutRight');
-                        }
-                    },
-                    100
-                );
-                if (index == 2) {
-					$('.name').addClass('fadeOutLeft').removeClass('fadeInLeft');
-                    $('.ico.thunder').addClass('bounceOutLeft').removeClass('bounceInLeft');
-                    $('.ico.mobile').addClass('bounceOutRight').removeClass('bounceInRight');
-                }
-				
-                setTimeout(
-                    function() {
-                        if (nextIndex == 3) {
-							$('.name').addClass('fadeInLeft').removeClass('fadeOutLeft');
-                        }
-                    },
-                    100
-                );
-                if (index == 3) {
-					$('.name').addClass('fadeOutLeft').removeClass('fadeInLeft');
-                }
-				
-                setTimeout(
-                    function() {
-                        if (nextIndex == 4) {
-							$('.name').addClass('fadeInLeft').removeClass('fadeOutLeft');
-                        }
-                    },
-                    100
-                );
-                if (index == 4) {
-					$('.name').addClass('fadeOutLeft').removeClass('fadeInLeft');
-                }
-				
-                setTimeout(
-                    function() {
-                        if (nextIndex == 5) {
-							$('.name').addClass('fadeInLeft').removeClass('fadeOutLeft');
-							$('.ico.code').addClass('bounceInDown').removeClass('bounceOutUp');
-							$('.ico.newsletter').addClass('bounceInUp').removeClass('bounceOutDown');
-                        }
-                    },
-                    100
-                );
-                if (index == 5) {
-					$('.name').addClass('fadeOutLeft').removeClass('fadeInLeft');
-					$('.ico.code').addClass('bounceOutUp').removeClass('bounceInDown');
-					$('.ico.newsletter').addClass('bounceOutDown').removeClass('bounceInUp');
-                }
-				
-            },
             afterRender: function(){
 				setTimeout (
 					function () {
